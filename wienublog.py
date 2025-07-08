c6b43d2066e7116c7726bed6f712859440d51f82
errorstrata <jonskevins0@gmail.com>
2025-07-08 13:41:13 +0300

    Update wienublog.py

--- wienublog.py
+++ wienublog.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 api = Api(app)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+print("DATABASE_URL:", os.environ.get('DATABASE_URL'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #app.config['SQLALCHEMY_ECHO'] = True
 db=SQLAlchemy(app)
